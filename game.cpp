3b812f1da3d5c470109267ea3d642e35832e9b1e
yincircle <yinheng304@gmail.com>
2017-10-02 10:52:48 +0800

    print

--- game.cpp
+++ game.cpp
@@ -2,8 +2,8 @@
 #include<stdlib.h>
  int map[20][20];//新建地图坐标
  struct food {
-	 int xpost = 0;
-	 int ypost = 0;
+	 int foodxpost = 0;
+	 int foodypost = 0;
 	 int FoodAlive = 1;
  };//定义食物的坐标和状态
  struct post {
@@ -16,3 +16,25 @@
 	 int snakealive = 1;
 	 post p[200]{};
  };//定义蛇的方向，长度，和所有蛇的位置
+ int main() {
+	 void print_map(); /*打印*/{
+		 int p, q;
+		 for (p = 0; p < 21; p++) {
+			 for (q = 0; q < 21; q++) {
+				 if (map[p][q] == 0/*0代表空白*/) {
+					 printf(" ");
+				 }
+				 else if (map[p][q] == 1/*1代表蛇*/) {
+					 printf("*");
+				 }
+				 else if (map[p][q] == 2/*2代表食物*/) {
+					 printf("#");
+				 }
+				 else if (map[p][q] == 3/*3代表边界*/) {
+					 printf("%");
+				 }
+			 }
+		 }
+
+	 }
+  }
